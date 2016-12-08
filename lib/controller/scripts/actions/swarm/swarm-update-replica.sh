244a06462f751146d3cabc5fda1288b8886ef803
GeunhoKim <geunho.khim@gmail.com>
2016-12-09 08:40:24 +0900

    no message

--- lib/controller/scripts/actions/swarm/swarm-update-replica.sh
+++ lib/controller/scripts/actions/swarm/swarm-update-replica.sh
@@ -14,6 +14,13 @@ SERVICENAME="$1"
 
 REPLICA_COUNT="$2"
 
-docker service scale $SERVICENAME=0
-sleep 5s
-docker service scale $SERVICENAME=$REPLICA_COUNT
+REGISTRY="52.187.69.164:5000"
+NETWORK_OVERLAY="gamma-network"
+
+docker service rm $SERVICENAME
+
+docker service create --replicas $REPLICA_COUNT --network $NETWORK_OVERLAY --name $SERVICENAME $REGISTRY/$SERVICENAME
+
+
+
+# docker service scale $SERVICENAME=$REPLICA_COUNT
